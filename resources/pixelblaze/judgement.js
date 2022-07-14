c0fadcf13462a97d52d3a69b5cd6c32343be3b1d
Austin Bourdier <austinbourdier@google.com>
2022-07-13 17:06:07 -0700

    judgment cleanup

--- resources/pixelblaze/judgement.js
+++ resources/pixelblaze/judgement.js
@@ -7,37 +7,72 @@ export function beforeRender(delta) {
 }
 
 export function render(index) {
-  if (timeSinceStart < 10000) {
-    if (Math.floor(timeSinceStart / 2000) % 2 === 0) {
+  if (timeSinceStart < 33000) {
+    if (Math.floor(timeSinceStart / 5000) % 2 === 0) {
         rgb(255, 255, 255)
     } else {
         rgb(255, 0, 0)
     }
-  } else if (timeSinceStart < 18000) {
-    if (Math.floor(timeSinceStart / 1000) % 2 === 0) {
-        rgb(255, 255, 255)
-    } else {
-        rgb(255, 0, 0)
-    }
-  } else if (timeSinceStart < 24000) {
+  } else if (timeSinceStart < 35000) {
     if (Math.floor(timeSinceStart / 500) % 2 === 0) {
         rgb(255, 255, 255)
     } else {
         rgb(255, 0, 0)
     }
-  } else if (timeSinceStart < 28000) {
-    if (Math.floor(timeSinceStart / 250) % 2 === 0) {
+  } else if (timeSinceStart < 36000) {
+    if (Math.floor(timeSinceStart / 400) % 2 === 0) {
         rgb(255, 255, 255)
     } else {
         rgb(255, 0, 0)
     }
-  } else if (timeSinceStart < 30000) {
-    if (Math.floor(timeSinceStart / 100) % 2 === 0) {
+  } else if (timeSinceStart < 37000) {
+    if (Math.floor(timeSinceStart / 300) % 2 === 0) {
         rgb(255, 255, 255)
     } else {
         rgb(255, 0, 0)
     }
-  } else {
+  } else if (timeSinceStart < 38000) {
+        if (Math.floor(timeSinceStart / 200) % 2 === 0) {
+            rgb(255, 255, 255)
+        } else {
+            rgb(255, 0, 0)
+        }
+    }
+    else if (timeSinceStart < 39000) {
+        if (Math.floor(timeSinceStart / 150) % 2 === 0) {
+            rgb(255, 255, 255)
+        } else {
+            rgb(255, 0, 0)
+        }
+    }
+    else if (timeSinceStart < 40000) {
+        if (Math.floor(timeSinceStart / 100) % 2 === 0) {
+            rgb(255, 255, 255)
+        } else {
+            rgb(255, 0, 0)
+        }
+    }
+    else if (timeSinceStart < 41000) {
+        if (Math.floor(timeSinceStart / 50) % 2 === 0) {
+            rgb(255, 255, 255)
+        } else {
+            rgb(255, 0, 0)
+        }
+    }
+    else if (timeSinceStart < 42000) {
+        if (Math.floor(timeSinceStart / 25) % 2 === 0) {
+            rgb(255, 255, 255)
+        } else {
+            rgb(255, 0, 0)
+        }
+    }
+    else if (timeSinceStart < 50000) {
+        if (Math.floor(timeSinceStart / 10) % 2 === 0) {
+            rgb(255, 255, 255)
+        } else {
+            rgb(255, 0, 0)
+        }
+    } else {
     // Show winning color
     if (judgementWinner() === "heaven") {
         rgb(255, 255, 255)
