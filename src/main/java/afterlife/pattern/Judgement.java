6072464ff8f77dd24f8db33e7789763786e6a58a
austinbourdier <austinbourdier@github.com>
2022-08-23 08:59:53 -0700

    everything but judgment

--- src/main/java/afterlife/pattern/Judgement.java
+++ src/main/java/afterlife/pattern/Judgement.java
@@ -45,70 +45,13 @@ public class Judgement extends IsolationPattern {
       cummulativeTimePassedSinceLastAnimationUpdate += deltaMs;
       cummulativeTimePassed += deltaMs;
       if (incrementIntervals.size() > currentIncrementPeriod && cummulativeTimePassedSinceLastIncrement > incrementIntervals.get(currentIncrementPeriod)) {
-          isAnimating = true;
           cummulativeTimePassedSinceLastIncrement = 0;
           currentIncrementPeriod += 1;
-      }
-      if (isAnimating) {
-        animate();
-      }
-      if (cummulativeTimePassed >= 35500 && cummulativeTimePassed < 51000) {
-
-        if (lx.engine.tempo.beat()) {
-              int color = colorList.get(isBecomingHeaven ? 0 : 1);
+          int color = colorList.get(isBecomingHeaven ? 0 : 1);
               for (LXModel cloud : model.children) {
                   setColor(cloud, color);
               }
               isBecomingHeaven = !isBecomingHeaven;
-          }
       }
-      if (cummulativeTimePassed > 51000 && cummulativeTimePassed < 57000) {
-        for (LXModel cloud : model.children) {
-                  setColor(cloud, Integer.valueOf(Color.TRANSLUCENT));
-              }
-      }
-      if (cummulativeTimePassed > 57000) {
-          if(AfterlifeMidi.hellCounter > AfterlifeMidi.heavenCounter) {
-              for (LXModel cloud : model.children) {
-                setColor(cloud, Integer.valueOf(Color.RED.getRGB()));
-            }
-          } else {
-            for (LXModel cloud : model.children) {
-                setColor(cloud, Integer.valueOf(Color.WHITE.getRGB()));
-            }
-          }
-      }
-  }
-
-  private void animate () {
-      if ((cummulativeTimePassedSinceLastAnimationUpdate > 50 && cummulativeTimePassed < 35500)) {
-            for (int index = 0; index < model.children.length; index++) {
-                LXModel cloud = model.children[index];
-                int stripLength = cloud.tags.contains("SMALL") ? 30 : 60;
-                if (cloudProgress.get(index) < stripLength - 1) {
-                    if (isBecomingHeaven) {
-                        for (LXModel strip : cloud.children) {
-                            setColor(strip.getPoints().get(cloudProgress.get(index)), Integer.valueOf(Color.WHITE.getRGB()));
-                        }
-                    } else {
-                        // turn to hell
-                        for (LXModel strip : cloud.children) {
-                            setColor(strip.getPoints().get(stripLength - 1 - cloudProgress.get(index)), Integer.valueOf(Color.RED.getRGB()));
-                        }
-                    }
-                    cloudProgress.set(index, cloudProgress.get(index) + 1);
-                } else {
-                    cloudProgress.set(index, 0);
-                    if (cloud.tags.contains("LARGE")) {
-                        isAnimating = false;
-                    }
-                    if (!isAnimating && index == 0) {
-                        isBecomingHeaven = !isBecomingHeaven;
-                    }
-                }
-            }
-            cummulativeTimePassedSinceLastAnimationUpdate = 0;
-      }
-
   }
 }
