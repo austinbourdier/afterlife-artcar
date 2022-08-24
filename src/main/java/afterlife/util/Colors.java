cdfb4927bff153688fe7b9ca6be726ea88862759
Eric Eakin <eric.eakin@getcruise.com>
2022-08-24 05:04:29 -0700

    Added Wakin with Eakin pattern

--- src/main/java/afterlife/util/Colors.java
+++ src/main/java/afterlife/util/Colors.java
@@ -14,6 +14,14 @@ public class Colors {
   public static int MAGENTA = Integer.valueOf(Color.MAGENTA.getRGB());
   public static int TRANSLUCENT = Integer.valueOf(Color.TRANSLUCENT);
 
+  public static int HEAVEN_BASELINE = Integer.valueOf(new Color(97, 148, 171).getRGB());
+  public static int HEAVEN = Integer.valueOf(new Color(107, 209, 255).getRGB());
+  public static int ULTRA_HEAVEN = Integer.valueOf(new Color(224, 245, 255).getRGB());
+
+  public static int HELL_BASELINE = Integer.valueOf(new Color(138, 92, 69).getRGB());
+  public static int HELL = Integer.valueOf(Color.RED.getRGB());
+  public static int ULTRA_HELL = Integer.valueOf(new Color(255, 88, 5).getRGB());
+  public static int NEUTRAL_GREEN = Integer.valueOf(new Color(7, 107, 31).getRGB());
 
 
 }
